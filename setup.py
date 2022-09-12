06c91a49bbaa2df1513ece6725db600dd0be5bfc
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-09-12 12:37:04 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+version = "0.0.1"
+
+setup(
+    packages=["skies"],
+    install_requires=[],
+    zip_safe=False,
+    name="skies",
+    version=version,
+    description="Stochastic Kinematically Informed Earthquake Sequences",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/brendanjmeade/skies",
+    author="Brendan Meade",
+    author_email="brendanjmeade@gmail.com",
+    license="MIT",
+    platforms=["any"],
+    classifiers=[],
+)
