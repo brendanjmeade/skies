635710939eba89dd136e5874baf1af50172cc0aa
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-09-12 22:14:33 -0400

    Just the frame work for moving functions out of the main notebook

---
+++ skies/skies.py
@@ -0,0 +1,14 @@
+import addict
+import copy
+import datetime
+import json
+import meshio
+import os
+import warnings
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+from importlib import reload
+from pprint import pprint
+from ismember import ismember
+import matplotlib
