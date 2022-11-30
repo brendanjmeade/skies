a980b449f078b72375dbd35c41d64a5cf93971d7
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-11-30 10:00:28 -0500

    Practice writing HDF output

--- test_stuff.py
+++ test_stuff.py
@@ -1,34 +1,23 @@
 import datetime
 import numpy as np
-import scipy
+import h5py
 
 # Matrix size and number of eigenvalues for truncated case
-n_pts = 1000
-n_eigenvalues = 100
+n_time_steps = 30
+n_values = 1000
+# fake_data = np.random.rand(n_time_steps, n_values)
 
-# Set of coordinates for synthetic distance calculation
-x = np.random.rand(n_pts)
-y = np.random.rand(n_pts)
-z = np.random.rand(n_pts)
-centroid_coordinates = np.array([x, y, z]).T
-distance_matrix = scipy.spatial.distance.cdist(
-    centroid_coordinates, centroid_coordinates, "euclidean"
-)
-distance_matrix = (distance_matrix - np.min(distance_matrix)) / np.ptp(distance_matrix)
-correlation_matrix = np.exp(-distance_matrix)
-
-# Full vs. truncated eigenvalue experiments
-start_time = datetime.datetime.now()
-eigenvalues_truncated, eigenvectors_truncated = scipy.linalg.eigh(
-    correlation_matrix,
-    subset_by_index=[n_pts - n_eigenvalues, n_pts - 1],
-)
-end_time = datetime.datetime.now()
-print(f"Truncated EV time: {(end_time - start_time)}")
+# Open hdf file for writing
+file_name = "test.hdf5"
+hf = h5py.File(file_name, "w")
 
 start_time = datetime.datetime.now()
-eigenvalues, eigenvectors = scipy.linalg.eigh(correlation_matrix)
+ds = hf.create_dataset("geometric_moment", shape=(n_time_steps, n_values), dtype=float)
+for i in range(n_time_steps):
+    ds[i, :] = np.random.rand(n_values)
 end_time = datetime.datetime.now()
-print(f"Full EV time     : {(end_time - start_time)}")
+print(f"HDF save time: {(end_time - start_time)}")
 
-print(np.allclose(eigenvalues[-n_eigenvalues:], eigenvalues_truncated))
+
+
+hf.close()
